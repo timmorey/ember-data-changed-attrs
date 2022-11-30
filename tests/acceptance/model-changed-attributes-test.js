769b9a1c578a5430d78dcd971b4a2507be98addd
Timothy Morey <tmorey@esri.com>
2022-11-30 11:09:45 -0500

    fix lint

--- tests/acceptance/model-changed-attributes-test.js
+++ tests/acceptance/model-changed-attributes-test.js
@@ -4,20 +4,40 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Acceptance | model changed attributes', function (hooks) {
   setupApplicationTest(hooks);
-  setupMirage(hooks)
+  setupMirage(hooks);
 
   test('Model.changedAttributes should be cleared after save', async function (assert) {
-    server.createList('thing', 10);
+    this.server.createList('thing', 10);
     const store = this.owner.lookup('service:store');
     const thing = await store.findRecord('thing', '1');
-    assert.deepEqual(thing.changedAttributes(), {}, 'changedAttributes initially empty')
-    thing.name = 'new name'
-    assert.ok(Object.keys(thing.changedAttributes()), ['name'], 'changedAttributes contains name key')
-    await thing.save()
-    assert.deepEqual(thing.changedAttributes(), {}, 'changedAttributes should be empty after save')
-    thing.description = 'its a thing'
-    assert.deepEqual(Object.keys(thing.changedAttributes()), ['description'], 'only description should be in changedAttributes')
-    await thing.save()
-    assert.deepEqual(thing.changedAttributes(), {}, 'changedAttributes should be empty after save')
+    assert.deepEqual(
+      thing.changedAttributes(),
+      {},
+      'changedAttributes initially empty'
+    );
+    thing.name = 'new name';
+    assert.deepEqual(
+      Object.keys(thing.changedAttributes()),
+      ['name'],
+      'changedAttributes contains name key'
+    );
+    await thing.save();
+    assert.deepEqual(
+      thing.changedAttributes(),
+      {},
+      'changedAttributes should be empty after save'
+    );
+    thing.description = 'its a thing';
+    assert.deepEqual(
+      Object.keys(thing.changedAttributes()),
+      ['description'],
+      'only description should be in changedAttributes'
+    );
+    await thing.save();
+    assert.deepEqual(
+      thing.changedAttributes(),
+      {},
+      'changedAttributes should be empty after save'
+    );
   });
 });
